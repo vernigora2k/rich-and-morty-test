8bc97100cf1de087a94e23b67d9421ad92f80c4f
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2021-02-27 22:49:42 +0200

    Added search bar and filter on character page

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,3 +1,4 @@
+import { set } from 'lodash'
 import React, { Fragment, useEffect, useContext, useState } from 'react'
 import { Context } from '../App'
 import Card from '../components/Card'
@@ -7,6 +8,10 @@ import './characters.scss'
 export const Characters = () => {
     const { itemsList, fetchData, nextPage, setItemsList } = useContext(Context)
     const [currentUrl, setCurrentUrl] = useState(urlCharacters)
+    const [searchName, setSearchName] = useState('')
+    const [searchStatus, setSearchStatus] = useState('')
+    const [searchSpecies, setSearchSpecies] = useState('')
+    const [searchGender, setSearchGender] = useState('')
 
     const handleScroll = () => {
         const currentPosition = window.pageYOffset
@@ -16,6 +21,29 @@ export const Characters = () => {
         if (allWindowHeight === clientWindowHeight + currentPosition) {
             setCurrentUrl(nextPage)
         }
+    }
+
+    const handleChangeName = (event) => {
+        setSearchName(event.target.value)
+    }
+
+    const handleChangeStatus = (event) => {
+        setSearchStatus(event.target.value)
+    }
+
+    const handleChangeSpecies = (event) => {
+        setSearchSpecies(event.target.value)
+    }
+
+    const handleChangeGender = (event) => {
+        setSearchGender(event.target.value)
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setItemsList([])
+        console.log(searchStatus)
+        setCurrentUrl(urlCharacters + '?name=' + searchName + searchStatus + '&species=' + searchSpecies + searchGender)
     }
 
     useEffect(() => setItemsList([]),[])
@@ -33,10 +61,30 @@ export const Characters = () => {
 
     return (
         <Fragment>
+            <nav className="characters__search-bar bg-primary">
+                <form className="characters__form" onSubmit={handleSubmit}>
+                    <span>name: </span>
+                    <input onChange={handleChangeName}></input>
+                    <select id="select-status" onChange={handleChangeStatus}>
+                        <option value="" selected disabled hidden>Choose status</option>
+                        <option value="&status=alive">alive</option>
+                        <option value="&status=dead">dead</option>
+                        <option value="&status=unknown">unknown</option>
+                    </select>
+                    <input onChange={handleChangeSpecies}></input>
+                    <select id="select-gender" onChange={handleChangeGender}>
+                        <option value="" selected disabled hidden>Choose gender</option>
+                        <option value="&gender=female">female</option>
+                        <option value="&gender=male">male</option>
+                        <option value="&gender=genderless">genderless</option>
+                        <option value="&gender=unknown">unknown</option>
+                    </select>
+                    <input type="submit" value="search" />
+                </form>
+            </nav>
             <h1>Characters</h1>
-            <div className="characters-items" id="characters-items">
+            <div className="characters__items" id="characters__items">
                 {itemsList.map((item, i) => {
-                    console.log(item)
                     return <Card item={item} key={i} />
                 })}
             </div>
